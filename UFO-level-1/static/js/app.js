a1f7e294cc3ceb2006989eb4e8a9efffc7e46abf
Patrick Barrett <pat.barrett926@gmail.com>
2021-07-29 16:34:38 -0500

    Final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -26,14 +26,13 @@ button.on('click', () => {
     d3.event.preventDefault();
     var inputDate = inputField1.property('value').trim();
     var inputCity = inputField2.property('value').toLowerCase().trim();
-    // filter matching user input 
+    // filter user input 
     var filterDate = data.filter(data => data.datetime === inputDate);
-    console.log(filterDate);
     var filterCity = data.filter(data => data.city === inputCity);
-    console.log(filterCity);
     var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
-    console.log(filterData);
 
+
+// populate table with filtered data
 tbody.html('');
 
 let response = {
@@ -47,42 +46,28 @@ if (response.filterData.length !== 0) {
         populate(filterCity) || populate(filterDate);
     }
     else {
-        tbody.append('tr').append('td').text('No UFO sigthings.');
+        tbody.append('tr').append('td').text('No UFO sigthings reported.');
     }
 });
 
 resetbtn.on('click', () => {
     tbody.html('')
     populate(data)
-    console.log('Reset Filter')
 })
-    /* var button = d3.select('#filter-btn');
 
-button.on('click', function() {
-    var dateInput = d3.select('#date-filter');
-    var dateValue = dateInput.property('value');
-
-    var filtered = tableData.filter(item => item.dateTime === dateValue);
-    tbody.html(``); */
-/*     tableData.forEach(item => {
-        var tr = tbody.append('tr');
-        tr.append('td').text(item.datetime);
-        tr.append('td').text(item.city);
-        tr.append('td').text(item.state);
-        tr.append('td').text(item.country);
-        tr.append('td').text(item.shape);
-        tr.append('td').text(item.durationMinutes);
-        tr.append('td').text(item.comments);
+// user input filtering triggered by 'Enter' key on keyboard
+var input = document.getElementById("datetime");
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("filter-btn").click();
+        }
     });
-    
-        filtered.forEach(item => {
-            var tr = tbody.append('tr');
-            tr.append('td').text(item.datetime);
-            tr.append('td').text(item.city);
-            tr.append('td').text(item.state);
-            tr.append('td').text(item.country);
-            tr.append('td').text(item.shape);
-            tr.append('td').text(item.durationMinutes);
-            tr.append('td').text(item.comments);
-        });
- */    
+
+var input = document.getElementById("city");
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("filter-btn").click();
+        }
+    });
